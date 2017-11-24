596a1f0e4637fe9c2932bcd0a1b1fc125b530b9c
xuebusi <xbs1019@126.com>
2017-11-25 00:08:24 +0800

    init project

---
+++ src/main/java/com/xuebusi/SpringkafkaconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.xuebusi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkafkaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkafkaconsumerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xuebusi/consumer/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.xuebusi.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消费者
+ */
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"app_log_1"})
+    public void receive1(String message){
+        System.out.println("app_log_1--消费消息:" + message);
+    }
+
+    @KafkaListener(topics = {"app_log_2"})
+    public void receive2(String message){
+        System.out.println("app_log_2--消费消息:" + message);
+    }
+
+    @KafkaListener(topics = {"app_log_3"})
+    public void receive3(String message){
+        System.out.println("app_log_3--消费消息:" + message);
+    }
+}
